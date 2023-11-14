e91b9c5a9761c5743559e85bab1f7f685df3bff2
ALanovaya <lanovaya.al6@gmail.com>
2023-11-14 16:57:16 +0300

    Add ci and template for library

---
+++ matrix-lib/app/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+import Matrix ()
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
---
+++ matrix-lib/src/Matrix.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+
+module Matrix where
+
