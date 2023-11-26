3146827d97d9790db47e354213c25dcfffcc38b2
ALanovaya <lanovaya.al6@gmail.com>
2023-11-27 01:45:33 +0300

    Define type of matrix

--- matrix-lib/src/Matrix.hs
+++ matrix-lib/src/Matrix.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Matrix where
 
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+-- Define the data type for n-dimensional matrices
+data Matrix a = Matrix [Matrix a] | Leaf a
+
+-- Generate a function to create an n-dimensional matrix
+mkMatrix :: Int -> Q Exp
+mkMatrix n
+  | n <= 0 = fail "Invalid matrix dimension"
+  | n == 1 = [| Leaf |]
+  | otherwise = [| Matrix |] `appE` [| replicateM |] `appE` [| n |] `appE` mkMatrix (n-1)
