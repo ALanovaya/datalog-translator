a2b1f5fca72ce62cbcc011421e68b440661940e0
ALanovaya <lanovaya.al6@gmail.com>
2024-05-25 20:24:38 +0300

    Implement interpreter for matrix ast

---
+++ matrix-interpreter/src/MatrixInterpreter.hs
@@ -0,0 +1,48 @@
+module MatrixInterpreter where
+
+import MatrixAST
+import Matrix
+
+-- Convert MatrixAST to Matrix
+fromMatrixAST :: MatrixOp a -> Matrix.Matrix a
+fromMatrixAST (MatrixConst matrixAST) = convert matrixAST
+  where
+    convert :: MatrixAST.Matrix a -> Matrix.Matrix a
+    convert (MatrixAST.Matrix [] _) = error "Empty dimensions list"
+    convert (MatrixAST.Matrix [_] [val]) = Matrix.Leaf val -- Single value case, Leaf node
+    convert (MatrixAST.Matrix (d:ds) vals)
+      | null ds = if length vals == d -- Base case for the last dimension
+                    then Matrix.Matrix [d] (map Matrix.Leaf vals)
+                    else error "Dimension values mismatch"
+      | otherwise =
+        let subMatrixSize = product ds
+            subMatrices = map (convert . MatrixAST.Matrix ds) (takeChunks subMatrixSize vals)
+        in Matrix.Matrix (d:ds) subMatrices
+fromMatrixAST _ = error "Unsupported MatrixOp type"
+
+takeChunks :: Int -> [a] -> [[a]]
+takeChunks _ [] = []
+takeChunks n xs = take n xs : takeChunks n (drop n xs)
+
+extendMatrix :: [Int] -> Matrix.Matrix Int -> Matrix.Matrix Int
+extendMatrix headMatrixDimensions matrix =
+  let currentDimensions = shape matrix
+      dimensionDiff = length headMatrixDimensions - length currentDimensions
+   in case compare dimensionDiff 0 of
+        GT -> increaseDimensions dimensionDiff matrix
+        LT ->
+          let sliceStarts = replicate (length currentDimensions) 0
+              sliceEnds =
+                headMatrixDimensions
+                  ++ drop (length headMatrixDimensions) currentDimensions
+           in slice sliceStarts sliceEnds matrix
+        EQ -> matrix
+
+evalMatrixOp :: MatrixOp Int -> Matrix.Matrix Int
+evalMatrixOp (MatrixConst matrix) = fromMatrixAST (MatrixConst matrix)
+evalMatrixOp (Multiply n m opA opB) =
+  multidimensionalMatrixMultiply n m (evalMatrixOp opA) (evalMatrixOp opB)
+evalMatrixOp (Add opA opB) =
+  addMatrices (evalMatrixOp opA) (evalMatrixOp opB)
+evalMatrixOp (Extend op dims) =
+  extendMatrix dims (evalMatrixOp op)
--- translator/src/MatrixAST.hs
+++ translator/src/MatrixAST.hs
@@ -10,9 +10,6 @@ data Matrix a = Matrix
 data MatrixOp a
   = MatrixConst (Matrix a)                      -- A constant matrix
   | Multiply Int Int (MatrixOp a) (MatrixOp a)  -- Matrix multiplication
-  | Transpose (MatrixOp a) Int Int              -- Matrix transpose
   | Extend (MatrixOp a) [Int]                   -- Matrix extensions
-  | Diagonalize (MatrixOp a) [Int] [Int]        -- Matrix diagonalization
   | Add (MatrixOp a) (MatrixOp a)               -- Matrix addition
-  | Delete (MatrixOp a) Int                     -- Matrix deletion of dimension
   deriving (Show, Eq)
