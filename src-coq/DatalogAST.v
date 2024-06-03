efd13eac0e52d833a094d0d19cab1bbc6f9e98dc
Alexandra Lanovaya <lanovaya.al6@gmail.com>
2024-06-03 18:38:38 +0300

    Fix type of datalog program

--- src-coq/DatalogAST.v
+++ src-coq/DatalogAST.v
@@ -32,7 +32,24 @@ Inductive Clause : Set :=
 
 (* Define a type for a DatalogProgram which holds a list of Clauses *)
 Record DatalogProgram : Set := mkDatalogProgram {
-  clauses : list Clause
+  clauses : list Clause;
+  chain_rule_extension : forall c : Clause, 
+    match c with
+    | ClauseFact _ => True
+    | ClauseRule r => 
+        let head_pred := r.(head).(predicate) in
+        let head_terms := r.(head).(terms) in
+        let body_atoms := r.(body) in
+        exists t : Term, 
+          (In t head_terms /\ 
+           forall b : Atom, 
+             In b body_atoms -> 
+             exists p : string, 
+               b.(predicate) = p /\ 
+               exists ts : list Term, 
+                 b.(terms) = ts /\ 
+                 In t ts)
+    end
 }.
 
 (* Define a type for a query (an atom representing the query to be evaluated) *)
